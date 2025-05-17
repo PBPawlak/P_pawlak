f449f23d4cfa514376495bb899c88280d794716d
Piotr Pawlak <ppawlak@edu.cdv.pl>
2025-05-17 13:22:53 +0200

    first embedder + dockloader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import random
-import time
 from openai import OpenAI
-import pandas
-from io import StringIO
+#from io import StringIO
 import pdfplumber
 
 
@@ -12,10 +9,36 @@ client = OpenAI(
   api_key = st.secrets["API_KEY"],
 )
 
-st.write("Streamlit loves LLMs! 🤖 [Build your own chat app](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps) in minutes, then make it powerful by adding images, dataframes, or even input widgets to the chat.")
+UPLOAD_FOLDER = "/uploads/docs"
 
-st.caption("Note that this demo app isn't actually connected to any LLMs. Those are expensive ;)")
+st.write("OpenRouter chatbot app by Piotr Pawlak")
 
+template = """
+You are an intelligent assistant for question-answering tasks. Use Polish language by default. Use the following context for answers
+If you don't know the asnwer, just say that you don't know, by stating "pomidor" or a tomato emoji. Use three senteces maximum to answer
+Question: {question}
+Context: {context}
+Answer:
+"""
+
+selected_model = "meta-llama/llama-3.2-1b-instruct:free"
+model = ChatOpenRouter(model_name=selected_model)
+
+def answer_question (question, documents, model):
+    context = "\n\n".join([doc["text"] for doc in documents])
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+    return chain.invoke({"question": question, "context": context})
+
+if "query" not in st.session_state:
+    st.session_state.query = ""
+if "answer" not in st.session_state:
+    st.session_state.answer = ""
+if "query" not in st.session_state:
+    st.session_state.query = ""
+
+
+###### PDF ######
 def extract_data(feed):
     data = []
     with pdfplumber.load(feed) as pdf:
@@ -24,43 +47,44 @@ def extract_data(feed):
             data.append(p.extract_tables())
     return None
 
-uploaded_file = st.file_uploader("Choose your pdf file", type="pdf")
-if uploaded_file is not None:
-    df = extract_data(uploaded_file)
+uploaded_files = st.file_uploader("Choose your pdf file", type="pdf", accept_multiple_files=Tr)
+if uploaded_files is not None:
+    df = extract_data(uploaded_files)
+###### PDF ######
 
-# Initialize chat history
+
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting! 👇"}]
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
+
 if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
+
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
+
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response in chat message container
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
         assistant_response = client.chat.completions.create(model=st.secrets["MODEL"], messages=st.session_state.messages)
-
         full_response=assistant_response.choices[0].message.content
-
-        # Simulate stream of response with milliseconds delay
-        # for chunk in assistant_response.split():
-        #     full_response += chunk + " "
-        #     time.sleep(0.05)
-        #     # Add a blinking cursor to simulate typing
-        #     message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
-    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-# st.write(st.secrets["MODEL"])
+
+
+if uploaded_files:
+    for upladed_file in upladed_files:
+        file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+    st.write("Files uploaded sucessfully!")
+    documents = load_documents_from_folder(UPLOAD_FOLDER)
+    st.session_state.faiss_index = create_index(documents)
+    st.write("Files retrieved successfully!")
+    st.session_state.retrieve_files = True
